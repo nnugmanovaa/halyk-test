039e53e066d3b09017fae8a844a92684a18745ad
nnugmanova <nnugmanovaa@gmail.com>
2022-06-03 18:56:07 +0600

    halyk test task

---
+++ src/main/java/com/test/sipmlePorject/SimplePorjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.sipmlePorject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePorjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePorjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/sipmlePorject/SimplePorjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sipmlePorject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePorjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
