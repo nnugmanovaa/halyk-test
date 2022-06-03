561f339643808a0530b822b44ed95577a086b800
nnugmanova <nnugmanovaa@gmail.com>
2022-06-03 18:56:07 +0600

    halyk test task

---
+++ src/test/java/com/test/sipmlePorject/SimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sipmlePorject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
